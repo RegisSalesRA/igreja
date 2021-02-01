7534ffee34de8b4a7d67e646e13f511c7208297e
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2021-02-01 14:43:43 -0300

    Update settings.py

--- Django_DRF/settings.py
+++ Django_DRF/settings.py
@@ -126,12 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
