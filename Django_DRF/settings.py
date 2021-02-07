a0b2ffe4c83ddcad9d10f1c2f527335219f171f1
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2021-02-07 00:01:54 -0300

    Update settings.py

--- Django_DRF/settings.py
+++ Django_DRF/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
