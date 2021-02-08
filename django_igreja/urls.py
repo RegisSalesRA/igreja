d6c8fa3274bd22d24df6f3f2734532894018bd9f
RegisSalesRA <regismsales@gmail.com>
2021-02-08 10:34:24 -0300

    Refactory

--- django_igreja/urls.py
+++ django_igreja/urls.py
@@ -1,4 +1,4 @@
-"""Django_DRF URL Configuration
+"""django_igreja URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- django_igreja/wsgi.py
+++ django_igreja/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_DRF project.
+WSGI config for django_igreja project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_DRF.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_igreja.settings')
 
 application = get_wsgi_application()
