7d436d40022431993f55e1dfd62d2089c44fa57a
RegisSalesRA <regismsales@gmail.com>
2021-02-14 23:37:37 -0300

    models events

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Eventos(models.Model):
+    foto = models.ImageField(null=True,blank=True)
+    nome = models.CharField(max_length=150)
+    data = models.DateField()
+    texto = models.TextField()
+    local = models.CharField(max_length=150)
+
+
+class Cultos(models.Model):
+    foto = models.ImageField(null=True,blank=True)
+    nome = models.CharField(max_length=150)
+    pastor = models.CharField(max_length=150)
+    data = models.DateField()
+
+class Novidades(models.Model):
+    foto = models.ImageField(null=True,blank=True)
+    nome = models.CharField(max_length=150)
+    texto = models.TextField()
+    
