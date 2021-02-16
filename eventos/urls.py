b7ccd393a97752839088c332f973d40bf01755c8
Regis Sales <regis@prodadospesquisa.com.br>
2021-02-16 13:06:11 -0300

    list events and cultos

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from eventos.views import eventos
+from eventos.views import eventos, cultos
 
 urlpatterns = [
     path('eventos', eventos, name='eventos'),
+    path('cultos', cultos, name='cultos'),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -4,9 +4,18 @@ from eventos.models import Eventos,Cultos,Novidades
 
 
 def eventos(request):
-    eventos = Cultos.objects.all()
+    eventos = Eventos.objects.all()
     context = {
         'eventos': eventos,
     }
 
-    return render(request, 'eventos.html', context)
+    return render(request, 'eventos/events.html', context)
+
+
+def cultos(request):
+    cultos = Cultos.objects.all()
+    context = {
+        'cultos':cultos
+    }
+    
+    return render(request, 'eventos/cultos.html', context)
