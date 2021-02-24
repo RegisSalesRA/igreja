62081716bf01b11bff0aa5b099d5c2ad8c92e201
RegisSalesRA <regismsales@gmail.com>
2021-02-24 01:22:32 -0300

    fixed time field

--- eventos/models.py
+++ eventos/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-
 # Create your models here.
-
+#models.TimeField
 
 class Eventos(models.Model):
 
     foto = models.ImageField(null=True, blank=True)
     nome = models.CharField(max_length=150)
-    data = models.DateTimeField(null=False, blank=False)
+    data = models.DateField(null=False, blank=False)
     texto = models.TextField()
     local = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -27,7 +26,7 @@ class Cultos(models.Model):
     tema = models.CharField(max_length=150)
     descricao = models.TextField()
     pastor = models.CharField(max_length=150)
-    data = models.DateTimeField(null=False, blank=False)
+    data = models.DateField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
