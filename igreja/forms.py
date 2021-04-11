4c1b9dd6f4d59dbaa6a05132638b459c728823a0
Régis Rommel Madeiro Sales <48102820+RegisSalesRA@users.noreply.github.com>
2021-04-11 05:32:14 -0300

    Update forms.py

--- igreja/forms.py
+++ igreja/forms.py
@@ -41,6 +41,9 @@ class SiginUpForm(UserCreationForm):
     last_name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(max_length=250, help_text='eg. youremail@gmail.com')
 
+    
+    
+    
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
